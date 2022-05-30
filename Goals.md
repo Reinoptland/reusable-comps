67bf03f6a9a5c546f8dc37e2eabae43e8ae736a8
Reinoptland <reinoptland@gmail.com>
2022-05-30 15:07:15 +0200

    Nest content in intermediate button using props.children

--- Goals.md
+++ Goals.md
@@ -3,10 +3,36 @@
 - [x] Button component
 - [x] Display different text
 - [x] Display different styling (variants)
-- [ ] Display a spinner on the button (sometimes)
+- [x] Display a spinner on the button (sometimes)
 
 ## Beginner example
 
+Approach
+
+- Just make a prop for everything!
+- Use JS variables -> plug them into the a `style` object
+
+Pros
+
+- Easy, straightforward
+- Pretty dynamic, features are implemented
+
+Cons
+
+- Which prop can I use (your colleague needs to go throught the code ☹️)
+- The button becomes complex quickly (with each new variant usecase more code is added)
+- We are not using the power of CSS to keep things simple (no seperation of styling)
+
 ## Intermediate example
+
+Improvements wanted
+
+- Less props, but still supporting all the usecases
+- If the props are created, at least make it clear which props can be used
+- Maybe use CSS 🤷
+
+Approach
+
+- Use opening & closing tags with `props.children` for nesting content inside elements
 
 ## My approach
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BeginnerButton from "./components/BeginnerButton/BeginnerButton";
+import IntermediateButton from "./components/IntermediateButton/IntermediateButton";
 
 function App() {
   return (
@@ -9,6 +10,18 @@ function App() {
         <BeginnerButton buttonText="Post!" displaySpinner={true} />
         <BeginnerButton buttonText="Edit" size="lg" />
         <BeginnerButton buttonText="Cancel" variant="secondary" />
+      </header>
+      <header className="App-header">
+        {/* Self closing, like <img /> */}
+        <IntermediateButton />
+        {/* Make the button have opening tag, closing, like <button></button> */}
+        <IntermediateButton>
+          Post! <a href="https://google.com">GOOGLE IT</a>
+        </IntermediateButton>
+        <IntermediateButton>
+          {true ? <progress value={50} max={100} /> : "Edit!"}
+        </IntermediateButton>
+        <IntermediateButton>Cancel!</IntermediateButton>
       </header>
     </div>
   );
